f4056b8605a9f5c9548ce31c94bb3ef053131680
mintxxxx <130197753+mintxxxx@users.noreply.github.com>
2024-01-03 11:30:02 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jtcx/data_set/kitti/lidar2rosbag/src
-# Build directory: /home/jtcx/data_set/kitti/lidar2rosbag/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lidar2rosbag_KITTI")
--- build/lidar2rosbag_KITTI/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jtcx/data_set/kitti/lidar2rosbag/src/lidar2rosbag_KITTI
-# Build directory: /home/jtcx/data_set/kitti/lidar2rosbag/build/lidar2rosbag_KITTI
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
