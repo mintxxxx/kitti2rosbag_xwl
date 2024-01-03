57bb47c9c24edf10d21cd43469960370148acc6a
xwl <1456284493@qq.com>
2024-01-03 11:22:02 +0800

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jtcx/data_set/kitti/lidar2rosbag/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/jtcx/data_set/kitti/lidar2rosbag/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jtcx/data_set/kitti/lidar2rosbag/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jtcx/data_set/kitti/lidar2rosbag/src:$ROS_PACKAGE_PATH"
---
+++ build/lidar2rosbag_KITTI/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jtcx/data_set/kitti/lidar2rosbag/src/lidar2rosbag_KITTI
+# Build directory: /home/jtcx/data_set/kitti/lidar2rosbag/build/lidar2rosbag_KITTI
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
